b70f8078655854a57d36d1faea3fc16a4560d788
phamtanphat <phatdroid94@gmail.com>
2021-12-27 18:45:45 +0700

    declare manisfest

--- app/src/main/java/com/example/broadcastreceiver18102021/MyLocationBroadCastReceiver.java
+++ app/src/main/java/com/example/broadcastreceiver18102021/MyLocationBroadCastReceiver.java
@@ -8,25 +8,35 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
 public class MyLocationBroadCastReceiver extends BroadcastReceiver implements LocationListener {
 
+    Context context;
+
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("BBB", Thread.currentThread().getName());
+        this.context = context;
         LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0, this);
     }
 
     @Override
     public void onLocationChanged(@NonNull Location location) {
+        Toast.makeText(context, "Lat " + location.getLatitude() + " , lon "+ location.getLongitude(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
 
