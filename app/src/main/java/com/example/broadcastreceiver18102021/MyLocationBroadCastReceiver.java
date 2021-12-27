6b9c064fef560b9bb02cd325c562d71b5ac21e5e
phamtanphat <phatdroid94@gmail.com>
2021-12-27 18:13:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/broadcastreceiver18102021/MyLocationBroadCastReceiver.java
@@ -0,0 +1,42 @@
+package com.example.broadcastreceiver18102021;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+
+public class MyLocationBroadCastReceiver extends BroadcastReceiver implements LocationListener {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("BBB", Thread.currentThread().getName());
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10, this);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+}
